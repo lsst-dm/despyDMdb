62dbebb9e8e156abf3b6f8200b3885cc4bc180e0
Felipe Menanteau <felipe@illinois.edu>
2015-01-15 16:01:39 +0000

    minor changes to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 import distutils
 from distutils.core import setup
 
+#######
+# For later if we need executable scripts
+#import glob
+#bin_files = glob.glob("bin/*.py") # + glob.glob("bin/*.txt")
+#######
+
 # The main call
 setup(name='despyDMdb',
       version ='0.1.0',
@@ -10,5 +16,7 @@ setup(name='despyDMdb',
       author_email = "mgower@illinois.edu",
       packages = ['despydmdb'],
       package_dir = {'': 'python'},
+      #scripts = bin_files,
+      #data_files=[('ups',['ups/despyDMdb.table'])], # optional
       )
 
