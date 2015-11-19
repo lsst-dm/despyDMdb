c196419fc91d4332f855c6edb9be58813265a778
Felipe Menanteau <felipe@illinois.edu>
2015-11-19 17:48:01 +0000

    getting ready for version 0.1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup
 
 # The main call
 setup(name='despyDMdb',
-      version ='0.1.0',
+      version ='0.1.2',
       license = "GPL",
       description = "common DESDM DB classes/functions",
       author = "Michelle Gower",
