e18284b4226bc4d54a49f475430cd2d50e6aade4
Mikolaj Kowalik <mxk@illinois.edu>
2017-10-17 10:35:59 -0500

    Clean up the code using autopep8

--- python/despydmdb/dbsemaphore.py
+++ python/despydmdb/dbsemaphore.py
@@ -31,7 +31,7 @@ class DBSemaphore ():
         self.semname = semname
         self.task_id = task_id
         self.slot = None
-        
+
         miscutils.fwdebug(3, "SEMAPHORE_DEBUG", "SEM - INFO - semname %s" % self.semname)
         miscutils.fwdebug(3, "SEMAPHORE_DEBUG", "SEM - BEG - db-specific imports")
         import despydmdb.desdmdbi as desdmdbi
@@ -41,7 +41,7 @@ class DBSemaphore ():
         miscutils.fwdebug(3, "SEMAPHORE_DEBUG", "SEM - BEG - db connection")
         self.dbh = desdmdbi.DesDmDbi(desfile, section)
         miscutils.fwdebug(3, "SEMAPHORE_DEBUG", "SEM - END - db connection")
-        
+
         curs = self.dbh.cursor()
 
         sql = 'select count(*) from semlock where name=%s' % self.dbh.get_named_bind_string('name')
@@ -50,11 +50,11 @@ class DBSemaphore ():
         if num_slots == 0:
             miscutils.fwdebug(0, "SEMAPHORE_DEBUG", "SEM - ERROR - no locks with name %s" % semname)
             raise ValueError('No locks with name %s' % semname)
-            
+
         self.id = self.dbh.get_seq_next_value('seminfo_seq')
-        self.dbh.basic_insert_row('seminfo', {'id': self.id, 'name': self.semname, 
-                               'request_time': self.dbh.get_current_timestamp_str(),
-                               'task_id': task_id, 'num_slots': num_slots})
+        self.dbh.basic_insert_row('seminfo', {'id': self.id, 'name': self.semname,
+                                              'request_time': self.dbh.get_current_timestamp_str(),
+                                              'task_id': task_id, 'num_slots': num_slots})
         self.dbh.commit()
 
         self.slot = curs.var(cx_Oracle.NUMBER)
@@ -62,60 +62,57 @@ class DBSemaphore ():
         trycnt = 1
         MAXTRIES = 5
         TRYINTERVAL = 10
-        while not done and trycnt <= MAXTRIES: 
+        while not done and trycnt <= MAXTRIES:
             try:
                 miscutils.fwdebug(3, "SEMAPHORE_DEBUG", "SEM - BEG - wait")
-                curs.callproc("SEM_WAIT",[self.semname, self.slot])
+                curs.callproc("SEM_WAIT", [self.semname, self.slot])
                 miscutils.fwdebug(3, "SEMAPHORE_DEBUG", "SEM - END - wait")
                 miscutils.fwdebug(3, "SEMAPHORE_DEBUG", "SEM - INFO - slot %s" % self.slot)
                 done = True
             except Exception as e:
-                miscutils.fwdebug(0, "SEMAPHORE_DEBUG", "SEM - ERROR - %s" % str(e)) 
+                miscutils.fwdebug(0, "SEMAPHORE_DEBUG", "SEM - ERROR - %s" % str(e))
 
                 time.sleep(TRYINTERVAL)
 
                 miscutils.fwdebug(3, "SEMAPHORE_DEBUG", "SEM - BEG - remake db connection")
                 self.dbh = desdmdbi.DesDmDbi(desfile, section)
                 miscutils.fwdebug(3, "SEMAPHORE_DEBUG", "SEM - END - remake db connection")
-        
+
                 curs = self.dbh.cursor()
                 self.slot = curs.var(cx_Oracle.NUMBER)
 
-                miscutils.fwdebug(3, "SEMAPHORE_DEBUG", "SEM - BEG - dequeue") 
-                curs.callproc("SEM_DEQUEUE",[self.semname, self.slot])
-                miscutils.fwdebug(3, "SEMAPHORE_DEBUG", "SEM - END - dequeue") 
+                miscutils.fwdebug(3, "SEMAPHORE_DEBUG", "SEM - BEG - dequeue")
+                curs.callproc("SEM_DEQUEUE", [self.semname, self.slot])
+                miscutils.fwdebug(3, "SEMAPHORE_DEBUG", "SEM - END - dequeue")
 
                 trycnt += 1
 
         if done:
             # need different connection to do the commit of the grant info as commit will release lock
             dbh2 = desdmdbi.DesDmDbi(desfile, section)
-            dbh2.basic_update_row('SEMINFO', 
+            dbh2.basic_update_row('SEMINFO',
                                   {'grant_time': dbh2.get_current_timestamp_str(),
-                                     'num_requests': trycnt,
-                                     'slot': self.slot}, 
+                                   'num_requests': trycnt,
+                                   'slot': self.slot},
                                   {'id': self.id})
             dbh2.commit()
-            
-        
-        
 
     def __del__(self):
         """ 
         Do the semaphore signal and close DB connection
         """
         if self.slot != None:
-            try: 
+            try:
                 miscutils.fwdebug(3, "SEMAPHORE_DEBUG", "SEM - BEG - signal")
                 curs = self.dbh.cursor()
-                curs.callproc("SEM_SIGNAL",[self.semname, self.slot])
+                curs.callproc("SEM_SIGNAL", [self.semname, self.slot])
                 miscutils.fwdebug(3, "SEMAPHORE_DEBUG", "SEM - END - signal")
-                self.dbh.basic_update_row('SEMINFO', 
+                self.dbh.basic_update_row('SEMINFO',
                                           {'release_time': self.dbh.get_current_timestamp_str()},
                                           {'id': self.id})
                 self.dbh.commit()
             except Exception as e:
-                miscutils.fwdebug(0, "SEMAPHORE_DEBUG", "SEM - ERROR - %s" % str(e)) 
+                miscutils.fwdebug(0, "SEMAPHORE_DEBUG", "SEM - ERROR - %s" % str(e))
 
         self.slot = None
         self.dbh.close()
--- python/despydmdb/desdmdbi.py
+++ python/despydmdb/desdmdbi.py
@@ -12,7 +12,7 @@ Modified more often than the lower-level despydb
 
 __version__ = "$Rev$"
 
-import re 
+import re
 import sys
 import copy
 import time
@@ -23,6 +23,7 @@ import despydb.desdbi as desdbi
 import despydmdb.dmdb_defs as dmdbdefs
 import despymisc.miscutils as miscutils
 
+
 class DesDmDbi (desdbi.DesDbi):
     """
     Build on base DES db class adding DB functions used across various DM projects
@@ -31,7 +32,7 @@ class DesDmDbi (desdbi.DesDbi):
     def __init__(self, desfile=None, section=None):
         desdbi.DesDbi.__init__(self, desfile, section, retry=True)
 
-    def exec_sql_expression (self, expression):
+    def exec_sql_expression(self, expression):
         """
         Execute an SQL expression or expressions.
 
@@ -39,20 +40,19 @@ class DesDmDbi (desdbi.DesDbi):
         expression or a list of such strings.  Return a sequence containing a
         result for each column.
         """
-        if hasattr (expression, '__iter__'):
-            s = ','.join (expression)
+        if hasattr(expression, '__iter__'):
+            s = ','.join(expression)
         else:
             s = expression
 
-        stmt = self.get_expr_exec_format () % s
-        cursor = self.cursor ()
-        cursor.execute (stmt)
-        res = cursor.fetchone ()
-        cursor.close ()
+        stmt = self.get_expr_exec_format() % s
+        cursor = self.cursor()
+        cursor.execute(stmt)
+        res = cursor.fetchone()
+        cursor.close()
         return res
 
-
-    def get_expr_exec_format (self):
+    def get_expr_exec_format(self):
         """
         Return a format string for a statement to execute SQL expressions.
 
@@ -72,7 +72,6 @@ class DesDmDbi (desdbi.DesDbi):
         """
         return self.con.get_expr_exec_format()
 
-    
     def get_metadata(self):
         sql = "select * from ops_metadata"
         curs = self.cursor()
@@ -93,7 +92,6 @@ class DesDmDbi (desdbi.DesDbi):
 
         curs.close()
         return result
-
 
     def get_all_filetype_metadata(self):
         """
@@ -133,24 +131,23 @@ class DesDmDbi (desdbi.DesDbi):
             ptr = result[ftype]['hdus'][info['file_hdu'].lower()]
             if info['status'].lower() not in ptr:
                 ptr[info['status'].lower()] = OrderedDict()
-                    
+
             ptr = ptr[info['status'].lower()]
             if info['derived'].lower() not in ptr:
                 ptr[info['derived'].lower()] = OrderedDict()
-                    
+
             ptr[info['derived'].lower()][info['file_header_name'].lower()] = info['column_name'].lower()
 
         curs.close()
-        
-        return result
 
+        return result
 
     def get_site_info(self):
         """ Return contents of ops_site and ops_site_val tables """
         # assumes foreign key constraints so cannot have site in ops_site_val that isn't in ops_site
-        
+
         site_info = self.query_results_dict('select * from ops_site', 'name')
-        
+
         sql = "select name,key,val from ops_site_val"
         curs = self.cursor()
         curs.execute(sql)
@@ -158,20 +155,18 @@ class DesDmDbi (desdbi.DesDbi):
             site_info[name][key] = val
         return site_info
 
-
     def get_archive_info(self):
         """ Return contents of ops_archive and ops_archive_val tables """
         # assumes foreign key constraints so cannot have archive in ops_archive_val that isn't in ops_archive
-        
+
         archive_info = self.query_results_dict('select * from ops_archive', 'name')
-        
+
         sql = "select name,key,val from ops_archive_val"
         curs = self.cursor()
         curs.execute(sql)
         for (name, key, val) in curs:
             archive_info[name][key] = val
         return archive_info
-
 
     def get_archive_transfer_info(self):
         """ Return contents of ops_archive_transfer and ops_archive_transfer_val tables """
@@ -183,21 +178,22 @@ class DesDmDbi (desdbi.DesDbi):
         for row in curs:
             if row[0] not in archive_transfer:
                 archive_transfer[row[0]] = OrderedDict()
-            archive_transfer[row[0]][row[1]] = OrderedDict({'transfer':row[2]})
+            archive_transfer[row[0]][row[1]] = OrderedDict({'transfer': row[2]})
 
         sql = "select src,dst,key,val from ops_archive_transfer_val"
         curs = self.cursor()
         curs.execute(sql)
         for row in curs:
             if row[0] not in archive_transfer:
-                miscutils.fwdebug(0, 'DESDBI_DEBUG', "WARNING: found info in ops_archive_transfer_val for src archive %s which is not in ops_archive_transfer" % row[0]) 
+                miscutils.fwdebug(
+                    0, 'DESDBI_DEBUG', "WARNING: found info in ops_archive_transfer_val for src archive %s which is not in ops_archive_transfer" % row[0])
                 archive_transfer[row[0]] = OrderedDict()
             if row[1] not in archive_transfer[row[0]]:
-                miscutils.fwdebug(0, 'DESDBI_DEBUG', "WARNING: found info in ops_archive_transfer_val for dst archive %s which is not in ops_archive_transfer" % row[1]) 
+                miscutils.fwdebug(
+                    0, 'DESDBI_DEBUG', "WARNING: found info in ops_archive_transfer_val for dst archive %s which is not in ops_archive_transfer" % row[1])
                 archive_transfer[row[0]][row[1]] = OrderedDict()
-            archive_transfer[row[0]][row[1]][row[2]] = row[3]    
+            archive_transfer[row[0]][row[1]][row[2]] = row[3]
         return archive_transfer
-
 
     def get_job_file_mvmt_info(self):
         """ Return contents of ops_job_file_mvmt and ops_job_file_mvmt_val tables """
@@ -210,14 +206,14 @@ class DesDmDbi (desdbi.DesDbi):
         for (site, home, target, mvmt) in curs:
             if home is None:
                 home = 'no_archive'
-             
+
             if target is None:
                 target = 'no_archive'
 
             if site not in info:
-                info[site]  = OrderedDict()
+                info[site] = OrderedDict()
             if home not in info[site]:
-                info[site][home]  = OrderedDict()
+                info[site][home] = OrderedDict()
             info[site][home][target] = OrderedDict({'mvmtclass': mvmt})
 
         sql = "select site,home_archive,target_archive,key,val from ops_job_file_mvmt_val"
@@ -226,17 +222,17 @@ class DesDmDbi (desdbi.DesDbi):
         for (site, home, target, key, val) in curs:
             if home is None:
                 home = 'no_archive'
-             
+
             if target is None:
                 target = 'no_archive'
 
             if (site not in info or
                 home not in info[site] or
-                target not in info[site][home]):
-                miscutils.fwdie("Error: found info in ops_job_file_mvmt_val (%s, %s, %s, %s, %s) which is not in ops_job_file_mvmt" % (site, home, target, key, val), 1) 
-            info[site][home][target][key] = val   
+                    target not in info[site][home]):
+                miscutils.fwdie("Error: found info in ops_job_file_mvmt_val (%s, %s, %s, %s, %s) which is not in ops_job_file_mvmt" % (
+                    site, home, target, key, val), 1)
+            info[site][home][target][key] = val
         return info
-
 
     def load_artifact_gtt(self, filelist):
         """ insert file artifact information into global temp table """
@@ -247,12 +243,12 @@ class DesDmDbi (desdbi.DesDbi):
 
         # make sure table is empty before loading it
         self.empty_gtt(dmdbdefs.DB_GTT_ARTIFACT)
-        
+
         colmap = [dmdbdefs.DB_COL_FILENAME, dmdbdefs.DB_COL_COMPRESSION,
                   dmdbdefs.DB_COL_MD5SUM, dmdbdefs.DB_COL_FILESIZE]
         rows = []
         for file in filelist:
-            miscutils.fwdebug(3, 'DESDBI_DEBUG', "file = %s" % file) 
+            miscutils.fwdebug(3, 'DESDBI_DEBUG', "file = %s" % file)
             fname = None
             comp = None
             md5sum = None
@@ -265,15 +261,16 @@ class DesDmDbi (desdbi.DesDbi):
                     fname = file[dmdbdefs.DB_COL_FILENAME.lower()]
                     comp = file[dmdbdefs.DB_COL_COMPRESSION.lower()]
                 elif dmdbdefs.DB_COL_FILENAME in file:
-                    (fname,comp) = miscutils.parse_fullname(file[dmdbdefs.DB_COL_FILENAME], parsemask)
+                    (fname, comp) = miscutils.parse_fullname(file[dmdbdefs.DB_COL_FILENAME], parsemask)
                 else:
-                    (fname,comp) = miscutils.parse_fullname(file[dmdbdefs.DB_COL_FILENAME.lower()], parsemask)
-                miscutils.fwdebug(3, 'DESDBI_DEBUG', "fname=%s, comp=%s"  % (fname,comp)) 
+                    (fname, comp) = miscutils.parse_fullname(
+                        file[dmdbdefs.DB_COL_FILENAME.lower()], parsemask)
+                miscutils.fwdebug(3, 'DESDBI_DEBUG', "fname=%s, comp=%s" % (fname, comp))
             elif 'fullname' in file:
-                (fname,comp) = miscutils.parse_fullname(file['fullname'], parsemask)
-                miscutils.fwdebug(3, 'DESDBI_DEBUG', "parse_fullname: fname=%s, comp=%s"  % (fname,comp)) 
+                (fname, comp) = miscutils.parse_fullname(file['fullname'], parsemask)
+                miscutils.fwdebug(3, 'DESDBI_DEBUG', "parse_fullname: fname=%s, comp=%s" % (fname, comp))
             else:
-                miscutils.fwdebug(3, 'DESDBI_DEBUG', "file=%s"  % (file)) 
+                miscutils.fwdebug(3, 'DESDBI_DEBUG', "file=%s" % (file))
                 raise ValueError("Invalid entry filelist (%s)" % file)
 
             if dmdbdefs.DB_COL_FILESIZE in file:
@@ -286,13 +283,13 @@ class DesDmDbi (desdbi.DesDbi):
             elif dmdbdefs.DB_COL_MD5SUM.lower() in file:
                 md5sum = file[dmdbdefs.DB_COL_MD5SUM.lower()]
 
-            miscutils.fwdebug(3, 'DESDBI_DEBUG', "row: fname=%s, comp=%s, filesize=%s, md5sum=%s"  % (fname,comp,filesize,md5sum)) 
-            rows.append({dmdbdefs.DB_COL_FILENAME:fname,dmdbdefs.DB_COL_COMPRESSION:comp,
-                         dmdbdefs.DB_COL_FILESIZE:filesize, dmdbdefs.DB_COL_MD5SUM:md5sum})
+            miscutils.fwdebug(3, 'DESDBI_DEBUG', "row: fname=%s, comp=%s, filesize=%s, md5sum=%s" %
+                              (fname, comp, filesize, md5sum))
+            rows.append({dmdbdefs.DB_COL_FILENAME: fname, dmdbdefs.DB_COL_COMPRESSION: comp,
+                         dmdbdefs.DB_COL_FILESIZE: filesize, dmdbdefs.DB_COL_MD5SUM: md5sum})
 
-        self.insert_many(dmdbdefs.DB_GTT_ARTIFACT,colmap,rows)
+        self.insert_many(dmdbdefs.DB_GTT_ARTIFACT, colmap, rows)
         return dmdbdefs.DB_GTT_ARTIFACT
-
 
     def load_filename_gtt(self, filelist):
         """ insert filenames into filename global temp table to use in join for later query """
@@ -300,14 +297,15 @@ class DesDmDbi (desdbi.DesDbi):
 
         # make sure table is empty before loading it
         self.empty_gtt(dmdbdefs.DB_GTT_FILENAME)
-        
-        colmap = [dmdbdefs.DB_COL_FILENAME,dmdbdefs.DB_COL_COMPRESSION]
+
+        colmap = [dmdbdefs.DB_COL_FILENAME, dmdbdefs.DB_COL_COMPRESSION]
         rows = []
         for file in filelist:
             fname = None
             comp = None
             if isinstance(file, basestring):
-                (fname,comp) = miscutils.parse_fullname(file, miscutils.CU_PARSE_FILENAME | miscutils.CU_PARSE_EXTENSION)
+                (fname, comp) = miscutils.parse_fullname(
+                    file, miscutils.CU_PARSE_FILENAME | miscutils.CU_PARSE_EXTENSION)
             elif isinstance(file, dict) and (dmdbdefs.DB_COL_FILENAME in file or dmdbdefs.DB_COL_FILENAME.lower() in file):
                 if dmdbdefs.DB_COL_COMPRESSION in file:
                     fname = file[dmdbdefs.DB_COL_FILENAME]
@@ -316,13 +314,15 @@ class DesDmDbi (desdbi.DesDbi):
                     fname = file[dmdbdefs.DB_COL_FILENAME.lower()]
                     comp = file[dmdbdefs.DB_COL_COMPRESSION.lower()]
                 elif dmdbdefs.DB_COL_FILENAME in file:
-                    (fname,comp) = miscutils.parse_fullname(file[dmdbdefs.DB_COL_FILENAME], miscutils.CU_PARSE_FILENAME | miscutils.CU_PARSE_EXTENSION)
+                    (fname, comp) = miscutils.parse_fullname(
+                        file[dmdbdefs.DB_COL_FILENAME], miscutils.CU_PARSE_FILENAME | miscutils.CU_PARSE_EXTENSION)
                 else:
-                    (fname,comp) = miscutils.parse_fullname(file[dmdbdefs.DB_COL_FILENAME.lower()], miscutils.CU_PARSE_FILENAME | miscutils.CU_PARSE_EXTENSION)
+                    (fname, comp) = miscutils.parse_fullname(
+                        file[dmdbdefs.DB_COL_FILENAME.lower()], miscutils.CU_PARSE_FILENAME | miscutils.CU_PARSE_EXTENSION)
             else:
                 raise ValueError("Invalid entry filelist (%s)" % file)
-            rows.append({dmdbdefs.DB_COL_FILENAME:fname,dmdbdefs.DB_COL_COMPRESSION:comp})
-        self.insert_many(dmdbdefs.DB_GTT_FILENAME,colmap,rows)
+            rows.append({dmdbdefs.DB_COL_FILENAME: fname, dmdbdefs.DB_COL_COMPRESSION: comp})
+        self.insert_many(dmdbdefs.DB_GTT_FILENAME, colmap, rows)
         return dmdbdefs.DB_GTT_FILENAME
 
     def load_id_gtt(self, idlist):
@@ -339,23 +339,22 @@ class DesDmDbi (desdbi.DesDbi):
 
     def empty_gtt(self, tablename):
         """ clean out temp table for when one wants separate commit/rollback control """
-        # could be changed to generic empty table function, for now wanted safety check 
+        # could be changed to generic empty table function, for now wanted safety check
 
         if 'gtt' not in tablename.lower():
             raise ValueError("Invalid table name for a global temp table (missing GTT)")
-    
+
         sql = "delete from %s" % tablename
         curs = self.cursor()
         curs.execute(sql)
         curs.close()
 
+    def create_task(self, name, info_table,
+                    parent_task_id=None, root_task_id=None, i_am_root=False,
+                    label=None, do_begin=False, do_commit=False):
+        """ insert a row into the task table and return task id """
 
-    def create_task(self, name, info_table, 
-                    parent_task_id = None, root_task_id = None, i_am_root = False,
-                    label = None, do_begin = False, do_commit = False):
-        """ insert a row into the task table and return task id """ 
-
-        row = {'name':name, 'info_table':info_table}
+        row = {'name': name, 'info_table': info_table}
 
         row['id'] = self.get_seq_next_value('task_seq') # get task id
 
@@ -363,13 +362,12 @@ class DesDmDbi (desdbi.DesDbi):
             row['parent_task_id'] = int(parent_task_id)
 
         if i_am_root:
-            row['root_task_id'] = row['id'] 
+            row['root_task_id'] = row['id']
         elif root_task_id is not None:
             row['root_task_id'] = int(root_task_id)
-        
-               
+
         if label is not None:
-            row['label'] = label 
+            row['label'] = label
 
         self.basic_insert_row('task', row)
 
@@ -381,9 +379,8 @@ class DesDmDbi (desdbi.DesDbi):
 
         return row['id']
 
-
     def begin_task(self, task_id, do_commit=False):
-        """ update a row in the task table with beginning of task info """ 
+        """ update a row in the task table with beginning of task info """
 
         updatevals = {'start_time': self.get_current_timestamp_str(),
                       'exec_host': socket.gethostname()}
@@ -393,22 +390,20 @@ class DesDmDbi (desdbi.DesDbi):
         if do_commit:
             self.commit()
 
-    
     def end_task(self, task_id, status, do_commit=False):
-        """ update a row in the task table with end of task info """ 
+        """ update a row in the task table with end of task info """
         wherevals = {}
-        wherevals['id'] = task_id 
+        wherevals['id'] = task_id
 
         updatevals = {}
         updatevals['end_time'] = self.get_current_timestamp_str()
         updatevals['status'] = status
 
-        self.basic_update_row ('task', updatevals, wherevals)
+        self.basic_update_row('task', updatevals, wherevals)
         if do_commit:
             self.commit()
 
-
-    def get_datafile_metadata(self,filetype):
+    def get_datafile_metadata(self, filetype):
         """ Gets a dictionary of all datafile (such as XML or fits table data files) metadata for the given filetype.
             Returns a list: [target_table_name,metadata]
         """
@@ -427,7 +422,7 @@ class DesDmDbi (desdbi.DesDbi):
                 order by md.attribute_name, md.POSITION"""
         result = OrderedDict()
         curs = self.cursor()
-        curs.execute(sql,{"afiletype":filetype})
+        curs.execute(sql, {"afiletype": filetype})
 
         tablename = None
         for row in curs:
@@ -447,11 +442,11 @@ class DesDmDbi (desdbi.DesDbi):
         curs.close()
         if tablename is None:
             raise ValueError('Invalid filetype - missing entries in datafile tables')
-        return [tablename,result]
+        return [tablename, result]
 
 
 #### Embedded simple test
-if __name__ ==  '__main__' :
+if __name__ == '__main__':
     dbh = DesDmDbi()
     print 'dbh = ', dbh
     if dbh.is_postgres():
@@ -464,7 +459,7 @@ if __name__ ==  '__main__' :
     print dbh.get_column_names('exposure')
 
     cursor = dbh.cursor()
-    cursor.execute ('SELECT count(*) from exposure')
+    cursor.execute('SELECT count(*) from exposure')
     row = cursor.fetchone()
     print 'Number exposures:', row[0]
     cursor.close()
